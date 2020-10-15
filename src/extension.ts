88f94b857b35cdec9829a98ddfc6f616d6f93956
Thiago Cordeiro <thiagocordeirooo@gmail.com>
2020-10-14 21:41:22 -0300

    Update README.md

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,10 @@
 import { commands, ExtensionContext, window, workspace } from "vscode";
 import GenerateComponentLib from "./GenerateComponentLib";
+import { WORKSPACE_NAME } from "./constants";
 
 export function activate(context: ExtensionContext) {
   const generateComponentsCommand = commands.registerCommand(
-    "generate-component-from-template.generateComponents",
+    `${WORKSPACE_NAME}.generateComponents`,
     generateComponents
   );
   context.subscriptions.push(generateComponentsCommand);
